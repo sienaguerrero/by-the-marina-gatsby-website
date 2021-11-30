3d48a7ea345215fdbcb7ae30c2ca9e853b8bb35d
Siena Maria Guerrero <sienaguerrero@gmail.com>
2021-11-30 03:35:41 -0800

    add new font styles

--- plugins/gatsby-plugin-top-layout/TopLayout.tsx
+++ plugins/gatsby-plugin-top-layout/TopLayout.tsx
@@ -13,10 +13,17 @@ export default function TopLayout(props) {
   return (
     <React.Fragment>
       <>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
         <link
           href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
           rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Dawning+of+a+New+Day&display=swap"
         />
         <link
           rel="stylesheet"
